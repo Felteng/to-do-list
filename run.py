3b4110a5e5381791a60b327ab2a466b0a5b84ccd
Felix Tenggren <felixtenggren1@gmail.com>
2024-04-22 10:11:22 +0000

    Add credentials for relevant spreadsheet

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Scope and credentials code from Code Institutes Love Sandwiches project
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPEAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPEAD_CLIENT.open("to_do_list")
+
