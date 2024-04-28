3c550d694886f82119c5575247245155a9aefec5
Felix Tenggren <felixtenggren1@gmail.com>
2024-04-28 09:43:26 +0000

    Add function to go back while editing

--- run.py
+++ run.py
@@ -54,12 +54,18 @@ def validate_index_input(action):
 
     Raise ValueError if the index input is smaller than 1
     or if it's not a number.
+
+    As for all editing inputs enter an empty string will take the user back.
     """
     while True:
         try:
             index = input(f"Which index would you like to {action}?\n")
+
             print("")
-            if int(index) < 1:
+            if index == "":
+                back_to_edit()
+
+            elif int(index) < 1:
                 raise ValueError
 
             index_has_content = validate_content_presence(int(index))
@@ -81,14 +87,17 @@ def validate_date_input():
     Parse deadline string into datetime object to compare and test format.
     Get current date and convert it to a string, then parse it back
     into a datetime object with strptime to compare against date_input.
+
+    As for all editing inputs enter an empty string will take the user back.
     """
     while True:
         try:
             deadline = input("Task deadline (yyyy-mm-dd):\n")
+            if deadline == "":
+                back_to_edit()
+
             print("")
-
             date_input = datetime.strptime(deadline, "%Y-%m-%d")
-
             date_now = str(datetime.now().date())
 
             if date_input < datetime.strptime(date_now, "%Y-%m-%d"):
@@ -104,23 +113,26 @@ def validate_task_input():
     """Call this function whenever an input for a task is needed.
 
     Valdidates the length of the task string to avoid bugs in the table
-    and assures ANY letter input is given with .lower().islower()
+    and assures ANY input is provided.
+
+    As for all editing inputs enter an empty string will take the user back.
     """
     while True:
         print(
             "If description is longer than 50 characters it will be cut to 50."
         )
         task = input("Task description:\n")
+        if task == "":
+            back_to_edit()
+
         if len(task) > 50:
             task = task[:50]
             print("Task description cut down.")
             break
 
-        elif not task.lower().islower():
-            print("Please provide a decscription.")
-
-        elif len(task) < 50:
+        else:
             break
+
     print("")
     return task
 
@@ -157,7 +169,7 @@ def confirm_action(action, task_index):
         if confirm == "y" or confirm == "yes":
             return "y"
 
-        elif confirm == "n" or confirm == "no":
+        elif confirm == "n" or confirm == "no" or confirm == "":
             return "n"
 
         else:
@@ -189,7 +201,7 @@ def list_decision():
             print("")
 
             if edit_decision == "y":
-                edit_list()
+                edit_task_list()
 
             elif edit_decision == "n":
                 list_decision()
@@ -239,13 +251,14 @@ def display_completed_list():
     list_decision()
 
 
-def edit_list():
+def edit_task_list():
     """Grab input from user as to how they would like to edit the to do list.
 
     Call the function relevant to the choice.
 
     If the input does not match options alert user and allow another attempt.
     """
+    print("Press enter at any point while editing to go back here.")
     edit_type = input(
                 "Type 'edit' to edit a list task\n"
                 "Type 'add' to add a task to the list\n"
@@ -265,14 +278,14 @@ def edit_list():
         complete_task()
 
     elif edit_type == "remove":
-        remove_task()
+        remove_task("To-do")
 
     elif edit_type == "none":
         list_decision()
 
     else:
         print(f"Did not recognize '{edit_type}'.")
-        edit_list()
+        edit_task_list()
 
 
 def edit_task():
@@ -306,12 +319,15 @@ def edit_task():
             TO_DO_SHEET.update_cell(task_index, 2, new_deadline)
             print("Task and deadline updated successfully\n")
             break
+        
+        elif cell_to_edit == "":
+            edit_task_list()
 
         else:
             print(f"Did not recognize '{cell_to_edit}'.")
 
     display_to_do_list()
-    edit_list()
+    edit_task_list()
 
 
 def add_task():
@@ -324,19 +340,21 @@ def add_task():
     TO_DO_SHEET.append_row([task, deadline])
     print("Task added successfully\n")
     display_to_do_list()
-    edit_list()
+    edit_task_list()
 
 
 def complete_task():
     """Ask the user which index they would like to complete.
 
     Get confirmation if the index is correct, if user is not sure,
-    return to edit_list(), if user is sure, proceed with completion.
+    return to edit_task_list(), if user is sure, proceed with completion.
 
     Add a value of 1 to task_index when changing a sheet
     since row 1 in the sheet has the headings.
 
     Add the date this function was carried out to the completed history sheet.
+
+    As for all editing inputs enter an empty string will take the user back.
     """
     task_index = validate_index_input("complete")
     confirm = confirm_action("complete", task_index)
@@ -350,29 +368,51 @@ def complete_task():
         TO_DO_SHEET.delete_rows(task_index + 1)
 
         print("Task completed successfully\n")
+    
+    elif confirm == "n":
+        back_to_edit()
 
     display_to_do_list()
-    edit_list()
+    edit_task_list()
 
 
-def remove_task():
+def remove_task(worksheet_name):
     """Ask the user which index they would like to remove.
 
+    Target the right worksheet with worksheet_name.
+
     Get confirmation if the index is correct, if user is sure,
-    proceed with removal, if user is not sure return to edit_list().
+    proceed with removal, if user is not sure return to edit_task_list().
 
     Add a value of 1 to task_index when changing the sheet
     since row 1 in the sheet has the headings.
+
+    As for all editing inputs enter an empty string will take the user back.
     """
     task_index = validate_index_input("remove")
     confirm = confirm_action("remove", task_index)
     if confirm == "y":
+        target_sheet = SHEET.worksheet(worksheet_name)
         print(f"Removing task {task_index}...")
-        TO_DO_SHEET.delete_rows(task_index + 1)
+        target_sheet.delete_rows(task_index + 1)
         print("Task removed successfully\n")
 
+    elif confirm == "n":
+        back_to_edit(worksheet_name)
+
     display_to_do_list()
-    edit_list()
+    edit_task_list()
+
+def back_to_edit(*origin_list):
+    """Call if user presses enter with no input or with 'n' while editing"""
+    print("Aborting editing and going back...")
+    if origin_list == "Completed":
+        display_completed_list()
+
+    else:
+        display_to_do_list()
+
+    edit_task_list()
 
 
 # Thanks to my mentor Rohit Sharma for suggesting this piece of code.
